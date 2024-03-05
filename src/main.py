78e19ca54010000168734878156c9d7e36a17919
jonoromo <jsromeo@calpoly.edu>
2024-03-05 11:02:27 -0800

    new files

--- src/main.py
+++ src/main.py
@@ -20,12 +20,10 @@ import motor_driver
 import encoder_reader
 import motor_controller
 import utime
-        
+
 def task1_fun():
     """!
-    Controls one motor to reach a specified angle as quickly as possible with no overshoot.
-    A finite state machine is implemented so that the motor only performs one step response
-    and then transitions to a "wait" state (State 2).
+    Camera processing.
     """
     
     t1_state = 0
@@ -33,6 +31,27 @@ def task1_fun():
     while(True):
         
         if (t1_state == 0):
+            
+            
+            
+        elif (t1_state == 1):
+            
+            
+                
+        elif (t1_state == 2):
+            
+            
+
+def task2_fun():
+    """!
+    Panning motor control
+    """
+    
+    t2_state = 0
+    
+    while(True):
+        
+        if (t2_state == 0):
             
             # Get references to the share and queue which have been passed to this task
             enPin = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP) # Initialize pin en_pin (PA10)
@@ -55,9 +74,9 @@ def task1_fun():
             
             con = motor_controller.Controller(0.06, 330)
             setpoint = 0
-            t1_state = 1
+            t2_state = 1
             
-        elif (t1_state == 1):
+        elif (t2_state == 1):
             
             #setpoint = 200
             setpoint = -200
@@ -71,65 +90,32 @@ def task1_fun():
             t1_state = 2
             yield 0
                 
-        elif (t1_state == 2):
-            
-            t1_state = 2
-            yield 0
-
-
-def task2_fun():
-    """!
-    Controls one motor to reach a specified angle as quickly as possible with no overshoot.
-    A finite state machine is implemented so that the motor only performs one step response
-    and then transitions to a "wait" state (State 2).
-    """
-    
-    t2_state = 0
-    
-    while(True):
-        
-        if (t2_state == 0):
-            
-            # Get references to the share and queue which have been passed to this task
-            enPin = pyb.Pin(pyb.Pin.board.PC1, pyb.Pin.OUT_PP) # Initialize pin en_pin (PA10)
-            in2_pin = pyb.Pin(pyb.Pin.board.PA1, pyb.Pin.OUT_PP) # Initialize pin in2_pin (PB5)
-            in1_pin = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP) # Initialize pin in1_pin (PB4)
-            timmy = pyb.Timer(5, freq=20000) # Initialize timer
-            ch_pos = timmy.channel(2, pyb.Timer.PWM, pin=in2_pin) # Initialize positive direction timer channel
-            ch_neg = timmy.channel(1, pyb.Timer.PWM, pin=in1_pin) # Initialize negative direction timer channel
-            
-            moe_2 = motor_driver.MotorDriver(enPin, in2_pin, in1_pin, timmy, ch_pos, ch_neg)
-            
-            pinA = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.AF_PP, pull=pyb.Pin.PULL_NONE, af=pyb.Pin.AF1_TIM2)
-            pinB = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.AF_PP, pull=pyb.Pin.PULL_NONE, af=pyb.Pin.AF1_TIM2)
-
-            timer = pyb.Timer(4, prescaler=1, period=65535)
-            chan_A = timer.channel(1, pyb.Timer.ENC_AB, pin=pinA)
-            chan_B = timer.channel(2, pyb.Timer.ENC_AB, pin=pinB)
-            
-            enc_2 = encoder_reader.Encoder(pinA, pinB, timer, chan_A, chan_B)
-            
-            con_2 = motor_controller.Controller(0.06, 100)
-            setpoint = 0
-            t2_state = 1
-            
-        elif (t2_state == 1):
-            
-            setpoint += 100
-            for i in range(60):
-                moe_2.set_duty_cycle(con_2.run(setpoint,enc_2.read()))
-                con_2.meas_time(utime.ticks_ms())
-                con_2.meas_pos(enc_2.read())
-                yield 0
-            moe_2.set_duty_cycle(0)   
-            con_2.print_results()
-            t2_state = 2
-            yield 0
-                
         elif (t2_state == 2):
             
             t2_state = 2
             yield 0
+
+
+def task3_fun():
+    """!
+    Servo control
+    """
+    
+    t3_state = 0
+    
+    while(True):
+        
+        if (t3_state == 0):
+            
+            
+            
+        elif (t3_state == 1):
+            
+            
+                
+        elif (t3_state == 2):
+            
+            
 
 
 # Create a share and a queue to test function and diagnostic printouts
@@ -139,6 +125,7 @@ def task2_fun():
 
 t1_state = 0
 t2_state = 0
+t3_state = 0
 
 # Create the tasks. If trace is enabled for any task, memory will be
 # allocated for state transition tracing, and the application will run out
@@ -146,9 +133,11 @@ t2_state = 0
 # debugging and set trace to False when it's not needed
 task1 = cotask.Task(task1_fun, name="Task_1", priority=1, period=10)
 task2 = cotask.Task(task2_fun, name="Task_2", priority=2, period=20)
+task3 = cotask.Task(task3_fun, name="Task_3", priority=3, period=20)
 
 cotask.task_list.append(task1)
 cotask.task_list.append(task2)
+cotask.task_list.append(task3)
 
 # Run the memory garbage collector to ensure memory is as defragmented as
 # possible before the real-time scheduler is started
---
+++ src/test_cam.py
@@ -0,0 +1,9 @@
+import utime as time
+from machine import Pin, I2C
+from mlx90640 import MLX90640
+from mlx90640.calibration import NUM_ROWS, NUM_COLS, IMAGE_SIZE, TEMP_K
+from mlx90640.image import ChessPattern, InterleavedPattern
+import mlx_cam
+
+if __name__ == "__main__":
+    mlx_cam.test_MLX_cam()
