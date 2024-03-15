0fc2e9bec9629637b8a68027359350f60dd9c26f
jonoromo <jsromeo@calpoly.edu>
2024-03-15 16:23:51 -0700

    comments added

--- src/main.py
+++ src/main.py
@@ -36,9 +36,7 @@ def task1_fun():
     
     while(True):
         
-        #print('Task 1 =',t1_state)
-        
-        # State 0: Set up camera
+        # State 0: INIT - Set up camera
         if (t1_state == 0):
             
             moved = 0
@@ -59,51 +57,45 @@ def task1_fun():
             else:
                 i2c_bus = I2C(1)
 
-            #print("MXL90640 Easy(ish) Driver Test")
-
             # Select MLX90640 camera I2C address, normally 0x33, and check the bus
             i2c_address = 0x33
             scanhex = [f"0x{addr:X}" for addr in i2c_bus.scan()]
-            #print(f"I2C Scan: {scanhex}")
 
             # Create the camera object and set it up in default mode
             camera = mlx_cam.MLX_Cam(i2c_bus)
-            #print(f"Current refresh rate: {camera._camera.refresh_rate}")
             camera._camera.refresh_rate = 10.0
-            #print(f"Refresh rate is now:  {camera._camera.refresh_rate}")
             i = 0
-            wait_cam = 200
+            wait_cam = 200 # total wait = wait_cam*task_period = 4 s
             t1_state = 1
             
-        # State 1: Wait for picture    
+        # State 1: TAKE PICTURE  
         elif (t1_state == 1):
-            # Keep trying to get an image; this could be done in a task, with
-            # the task yielding repeatedly until an image is available
             
-            if i < wait_cam:
-                i += 1
+            # Keep trying to get an image
+            
+            if i < wait_cam: # cooperatively holds Task 1 in this state until 5 seconds
+                i += 1       # has passed and a picture should be taken
             else:
                 image = None
                 while not image:
-                    image = camera.get_image_nonblocking()
+                    image = camera.get_image_nonblocking() # take picture
                 t1_state = 2
             yield 0
         
-        # State 2: Get position value
+        # State 2: DATA PROCESSING
         elif (t1_state == 2):
-            print('FREEZE')
-            val = camera.get_csv(image, limits=(0, 99))
-            my_share.put(round(val*100))
+            print('FREEZE')                             # runs function in camera class
+            val = camera.get_csv(image, limits=(0, 99)) # to get average hot pixel value
+            my_share.put(round(val*100)) # magnifies pixel value to store as a whole number  
             print('VAL',val)
             gc.collect()
-            #print(f"Memory: {gc.mem_free()} B free")
             t1_state = 3
             yield 0
          
-        # State 3: Camera wait state 
+        # State 3: WAIT
         elif (t1_state == 3):
             
-            t1_state = 3
+            t1_state = 3 # holds camera task, no more pictures to be taken
             yield 0
             
 
@@ -116,112 +108,128 @@ def task2_fun():
     
     while(True):
         
-        #print('Task 2 =',t2_state)
-        
+        # State 0: INIT - Set up motors
         if (t2_state == 0):
             
-            enPin = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP) # Initialize pin en_pin (PA10)
-            in2_pin = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP) # Initialize pin in2_pin (PB5)
-            in1_pin = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP) # Initialize pin in1_pin (PB4)
+            # Panning Motor Initialization
+            enPin = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP) # Initialize pin en_pin
+            in2_pin = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP) # Initialize pin in2_pin
+            in1_pin = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP) # Initialize pin in1_pin
             timmy = pyb.Timer(3, freq=20000) # Initialize timer
             ch_pos = timmy.channel(2, pyb.Timer.PWM, pin=in2_pin) # Initialize positive direction timer channel
             ch_neg = timmy.channel(1, pyb.Timer.PWM, pin=in1_pin) # Initialize negative direction timer channel
-            
             moe = motor_driver.MotorDriver(enPin, in2_pin, in1_pin, timmy, ch_pos, ch_neg)
             
+            # Panning Motor Encoder Initialization
             pinA = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.AF_PP, pull=pyb.Pin.PULL_NONE, af=pyb.Pin.AF1_TIM2)
             pinB = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.AF_PP, pull=pyb.Pin.PULL_NONE, af=pyb.Pin.AF1_TIM2)
-
             timer = pyb.Timer(4, prescaler=1, period=65535)
             chan_A = timer.channel(1, pyb.Timer.ENC_AB, pin=pinA)
             chan_B = timer.channel(2, pyb.Timer.ENC_AB, pin=pinB)
-            
             enc = encoder_reader.Encoder(pinA, pinB, timer, chan_A, chan_B)
             
-            start = 730
-            wait_fire = 350
+            # Create controller instance, kp = 0.075, ki = 0.001
             con = motor_controller.Controller(0.075, 0.001, start)
-            setpoint = start
-            t2_state = 1
-            moved = 0
             
-            enPin_f = pyb.Pin(pyb.Pin.board.PC1, pyb.Pin.OUT_PP) # Initialize pin en_pin (PA10)
-            in2_pin_f = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP) # Initialize pin in2_pin (PB5)
-            in1_pin_f = pyb.Pin(pyb.Pin.board.PA1, pyb.Pin.OUT_PP) # Initialize pin in1_pin (PB4)
+            start = 730       # encoder value for 180 degree turn
+            wait_fire = 350   # total wait = wait_fire*task_period = 3.5 s
+            setpoint = start  # define setpoint variable
+            moved = 0         # create variable to track number of motor movements
+            
+            # Flywheel PWM Initialization
+            enPin_f = pyb.Pin(pyb.Pin.board.PC1, pyb.Pin.OUT_PP) # Initialize pin en_pin
+            in2_pin_f = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP) # Initialize pin in2_pin
+            in1_pin_f = pyb.Pin(pyb.Pin.board.PA1, pyb.Pin.OUT_PP) # Initialize pin in1_pin
             timmy_f = pyb.Timer(5, freq=20000) # Initialize timer
             ch_pos_f = timmy_f.channel(1, pyb.Timer.PWM, pin=in2_pin_f) # Initialize positive direction timer channel
             ch_neg_f = timmy_f.channel(2, pyb.Timer.PWM, pin=in1_pin_f) # Initialize negative direction timer channel
-            
             fly = motor_driver.MotorDriver(enPin_f, in2_pin_f, in1_pin_f, timmy_f, ch_pos_f, ch_neg_f)
+            
+            # Turn on nerf gun flywheel
             fly.set_duty_cycle(50)
             
+            t2_state = 1
+        
+        # State 1: MOVE/FIRE
         elif (t2_state == 1):
             
-            con.clear_esum(0)
-            hold = 0
+            con.clear_esum(0) # clears esum (for integral control)
+            hold = 0          # reset hold value
             for i in range(80):
-                moe.set_duty_cycle(con.run(setpoint,enc.read()))
+                moe.set_duty_cycle(con.run(setpoint,enc.read())) # provide PWM to panning motor
                 print(enc.read())
-                if abs( setpoint - enc.read() ) < 15:
-                    if hold > 4:
-                        print('DONE')
-                        break
-                    else:
+                if abs( setpoint - enc.read() ) < 10: # if the error is less than 10 encoder
+                    if hold > 4:                      # values, then this if statement is
+                        print('DONE')                 # entered. If the error is less than 10
+                        break                         # for 5 consecutive calls, the control
+                    else:                             # loop breaks.
                         hold += 1
                       
                 yield 0
-            moe.set_duty_cycle(0)
+            moe.set_duty_cycle(0) # set PWM to 0 when motor has reached the setpoint
             old_sp = setpoint
             t2_state = 2
             n = 0
             
-            if moved == 1:
+            if moved == 1: # only happens after the motor has made its aim correction
                 
                 time.sleep_ms(100)
-                pinC7 = pyb.Pin(pyb.Pin.board.PC7, pyb.Pin.OUT_PP)
+                pinC7 = pyb.Pin(pyb.Pin.board.PC7, pyb.Pin.OUT_PP) # initialize PC7 for output
                 pinC7.value(0)
                 time.sleep_ms(20)
-
+                
+                # 20 ms pulse with 2 ms high - pull trigger in
                 pinC7.value(1)
                 time.sleep_us(2000)
                 pinC7.value(0)
                 time.sleep_ms(18)
-
+                
+                # Wait to allow trigger to move through full trigger pull
                 time.sleep_ms(300)
-
+                
+                # Rapid on-off to interrupt previous signal pulse
                 pinC7.value(1)
                 pinC7.value(0)
-
+                
+                # 20 ms pulse with 0.5 ms high - release trigger
                 pinC7.value(1)
                 time.sleep_us(500)
                 pinC7.value(0)
                 time.sleep_ms(19)
-
+                
+                # Wait to allow servo to fully release the trigger
                 time.sleep_ms(100)
-
+                
+                # Rapid on-off to stop the servo at trigger edge
                 pinC7.value(1)
                 pinC7.value(0)
+                
+                # Wait, then set flywheel to off
                 time.sleep_ms(200)
                 fly.set_duty_cycle(0)
-                moved += 1
+                moved += 1 # increases moved so that the gun doesn't fire a second time
                 
             else:
-                moved += 1
+                moved += 1 # increases moved so that the firing sequence will happen
+                           # after the movement from camera data has occured
             
             yield 0
-                
+        
+        # State 2: WAIT
         elif (t2_state == 2):
             
-            if n < wait_fire:
-                n += 1
+            if n < wait_fire: # cooperatively holds motor task for 5 seconds until
+                n += 1        # the camera has taken its picture and processed the data
             else:
-                val = my_share.get()
+                val = my_share.get() # assigns share data as val
+                
+                # Translation of camera data to new setpoint
                 if val < 1450:
-                    setpoint = enc.read()+0.055*(val-1450)
+                    setpoint = enc.read()+0.055*(val-1450) # Calibration for moving left
                 else:
-                    setpoint = enc.read()+0.075*(val-1450)
+                    setpoint = enc.read()+0.075*(val-1450) # Calibration for moving right
                 print('SP',setpoint)
-                con.set_Ki(0.1)
+                con.set_Ki(0.1) # Set higher ki value for smaller movements
                 t2_state = 1
             yield 0
 
@@ -230,12 +238,6 @@ def task2_fun():
 my_share = task_share.Share ('h', name="My Share")
  
 # In another task, read data from the share
-
-
-# Create a share and a queue to test function and diagnostic printouts
-# share0 = task_share.Share('h', thread_protect=False, name="Share 0")
-# q0 = task_share.Queue('L', 16, thread_protect=False, overwrite=False,
-#                       name="Queue 0")
 
 t1_state = 0
 t2_state = 0
@@ -260,11 +262,5 @@ while True:
         cotask.task_list.pri_sched()
     except KeyboardInterrupt:
         break
-
-# Print a table of task data and a table of shared information data
-# print('\n' + str (cotask.task_list))
-# print(task_share.show_all())
-# print(task1.get_trace())
-# print('')
 
     
